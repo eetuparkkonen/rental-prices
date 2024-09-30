651287af46fbc5061bf44736ce6d0b4423226edc
Eetu Parkkonen <ede.parkkonen@gmail.com>
2024-09-30 12:34:31 +0300

    fixes

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="chartType === 'table'">
+  <div>
     <v-table class="ml-auto mr-auto">
       <thead>
         <tr class="text-center">
@@ -14,8 +14,8 @@
       <tbody>
         <tr v-for="(value, i) in data" class="text-center">
           <td>
-            {{ value.value[0] ?? "Arvoa ei löytynyt" }}
-            {{ value.value[0] ? " €" : "" }}
+            {{ value.value[0] ?? 'Arvoa ei löytynyt' }}
+            {{ value.value[0] ? ' €' : '' }}
           </td>
           <td>
             {{ Object.keys(value.dimension.Vuosineljännes.category.label)[0] }}
@@ -32,27 +32,16 @@
       </tbody>
     </v-table>
   </div>
-  <div v-else>
-    <div id="chart" v-if="data && data.length && series && chartOptions">
-      <apexchart
-        type="line"
-        height="350"
-        :options="chartOptions"
-        :series="series"
-      >
-      </apexchart>
-    </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import { Root } from "@/types/rentals";
-import { computed } from "vue";
+import { Root } from '@/types/rentals';
+import { computed } from 'vue';
 
 const props = defineProps<{
   data: Root[] | undefined;
   squareMeters: number[];
-  chartType: "line" | "table";
+  chartType: 'line' | 'table';
 }>();
 
 const calculatedPrice = (sqm: number, price: number) => {
@@ -61,21 +50,21 @@ const calculatedPrice = (sqm: number, price: number) => {
 
 // Create computed for series
 const series = computed(() => {
-  return [{
-    name: "Vuokrat",
-    data:
-      props.data?.map((rent) => {
-        return (
-          calculatedPrice(props.squareMeters[0], rent?.value?.[0] ?? 0) ?? 0
-        );
-      }) ?? [],
-  }];
+  return [
+    {
+      name: 'Vuokrat',
+      data:
+        props.data?.map((rent) => {
+          return calculatedPrice(props.squareMeters[0], rent?.value?.[0] ?? 0) ?? 0;
+        }) ?? [],
+    },
+  ];
 });
 
 const chartOptions = {
   chart: {
     height: 350,
-    type: "line",
+    type: 'line',
     zoom: {
       enabled: false,
     },
@@ -85,31 +74,54 @@ const chartOptions = {
     enabled: false,
   },
   stroke: {
-    curve: "smooth",
+    curve: 'smooth',
   },
   markers: {
     size: 6,
   },
   title: {
     text: `Vuokra neljännesvuosittain ${props.squareMeters[0]}m², ${props.data?.[1]?.dimension.Postinumero.category.label}`,
-    align: "left",
+    align: 'left',
   },
   grid: {
     row: {
-      colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
       opacity: 0.5,
     },
   },
   xaxis: {
     categories: [
-    '2015Q4', '2016Q1', '2016Q2', '2016Q3', '2016Q4',
-    '2017Q1', '2017Q2', '2017Q3', '2017Q4',
-    '2018Q1', '2018Q2', '2018Q3', '2018Q4',
-    '2019Q1', '2019Q2', '2019Q3', '2019Q4',
-    '2020Q1', '2020Q2', '2020Q3', '2020Q4',
-    '2021Q1', '2021Q2', '2021Q3', '2021Q4',
-    '2022Q1', '2022Q2', '2022Q3', '2022Q4',
-    '2023Q1', '2023Q2'
+      '2015Q4',
+      '2016Q1',
+      '2016Q2',
+      '2016Q3',
+      '2016Q4',
+      '2017Q1',
+      '2017Q2',
+      '2017Q3',
+      '2017Q4',
+      '2018Q1',
+      '2018Q2',
+      '2018Q3',
+      '2018Q4',
+      '2019Q1',
+      '2019Q2',
+      '2019Q3',
+      '2019Q4',
+      '2020Q1',
+      '2020Q2',
+      '2020Q3',
+      '2020Q4',
+      '2021Q1',
+      '2021Q2',
+      '2021Q3',
+      '2021Q4',
+      '2022Q1',
+      '2022Q2',
+      '2022Q3',
+      '2022Q4',
+      '2023Q1',
+      '2023Q2',
     ],
   },
 };
@@ -117,21 +129,20 @@ const chartOptions = {
 
 <style lang="scss">
 thead {
-    tr {
-        background-color: #243640 !important;
-        th {
-            color: white !important;
-            font-size: 1.1em;
-        }
+  tr {
+    background-color: #243640 !important;
+    th {
+      color: white !important;
+      font-size: 1.1em;
     }
-
+  }
 }
 
 tr:nth-child(odd) {
-    background-color: rgb(var(--v-theme-tertiary));
+  background-color: rgb(var(--v-theme-tertiary));
 }
 
 tr:nth-child(even) {
-    background-color: rgb(var(--v-theme-background));
+  background-color: rgb(var(--v-theme-background));
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from '@/App.vue'
-import '@/assets/styles/main.scss'
-import vuetify from '@/plugins/vuetify'
-import VueApexCharts from "vue3-apexcharts";
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import '@/assets/styles/main.scss';
+import vuetify from '@/plugins/vuetify';
+import VueApexCharts from 'vue3-apexcharts';
 
-createApp(App).use(vuetify).use(VueApexCharts).mount('#app')
+createApp(App).use(vuetify).use(VueApexCharts).mount('#app');
